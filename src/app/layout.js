091437e9943c5ae2176882c2dfda8f38a5545df7
NongFirstTH <TanabodeeChannel@gmail.com>
2023-08-07 15:46:24 +0700

    OK

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Tanabodee Srikhampa"
+            studentId="650610766"
           />
         </div>
       </body>
